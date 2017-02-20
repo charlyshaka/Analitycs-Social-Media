0138debc950be2563c3b40e6d36c6a16fac856fe
Carlos Antonio Lara Verduzco <charly_0666@hotmail.com>
2017-02-20 11:37:26 -0800

    Se agrego modelos de topicos
    Se agrego una grafica mas para ver el analsis de Topicos

--- usertimeline.R
+++ usertimeline.R
@@ -11,7 +11,7 @@ setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 ##### Limpieza de Texto
 
 ####### 
-tweets <- userTimeline("marianaSalas", n = 3000)
+tweets <- userTimeline("marianaSalas", n = 4000)
 tweets.df <- twListToDF(tweets)
 library(tm)
 
@@ -21,10 +21,10 @@ myCorpus <- tm_map(myCorpus, content_transformer(tolower))
 
 removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
 myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
-
+# remove anything other than English letters or space
 removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
 myCorpus <- tm_map(myCorpus, content_transformer(removeNumPunct))
-
+# remove stopwords
 
 # Remueve paralbras inecesarias
 myStopwords <- c(stopwords("spanish"), "un", "no","la","el")
@@ -32,7 +32,7 @@ myStopwords <- c(stopwords("spanish"), "un", "no","la","el")
 myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
 # remove extra el espacio extra
 myCorpus <- tm_map(myCorpus, stripWhitespace)
-
+# keep a copy for stem completion later
 myCorpusCopy <- myCorpus
 
 myCorpus <- tm_map(myCorpus, stemDocument)
@@ -45,7 +45,7 @@ tdm
 (freq.terms <- findFreqTerms(tdm, lowfreq=3))
 
 term.freq <- rowSums(as.matrix(tdm))
-term.freq <- subset(term.freq, term.freq >=3)
+term.freq <- subset(term.freq, term.freq >=100)
 df <- data.frame(term = names(term.freq), freq = term.freq)
 library(ggplot2)
 
@@ -53,13 +53,16 @@ ggplot(df, aes(x=term, y=freq)) + geom_bar(stat = "identity") + xlab("Terms") + 
 
 ##### Lluvia de palabras
 library(wordcloud)
+set.seed(1234)
+
 
 m <- as.matrix(tdm)
 # calculate the frequency of words and sort it by frequency
 word.freq <- sort(rowSums(m), decreasing = T)
 wordcloud(words = names(word.freq), freq = word.freq, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
-#######Analisis Cluster
+
+# remove sparse terms
 tdm2 <- removeSparseTerms(tdm, sparse = 0.95)
 m2 <- as.matrix(tdm2)
 # cluster terms
@@ -67,7 +70,7 @@ distMatrix <- dist(scale(m2))
 fit <- hclust(distMatrix, method = "ward.D")
 
 plot(fit)
-rect.hclust(fit, k = 6) # 6 Nucleos
+rect.hclust(fit, k = 6) # cut tree into 6 clusters 
 
 m3 <- t(m2) # transpose the matrix to cluster documents (tweets)
 set.seed(122) # set a fixed random seed
@@ -94,11 +97,22 @@ for (i in 1:k){
   s <- sort(kmeansResult$centers[i, ],decreasing = T)
   cat(names(s)[1:5], "\n")
 }
-          
- (n.tweet <- length(tweets))
- ##TopicModels
+  
+(n.tweet <- length(tweets))
+
+##TopicModels, modelos de topicos para descubrir
 dtm <- as.DocumentTermMatrix(tdm)
 library(topicmodels)
 lda <- LDA(dtm, k = 8)
 term <- terms(lda, 7) # first 7 terms of every topic
 (term <- apply(term, MARGIN = 2, paste, collapse = ", "))
+library("lda")
+
+term <- apply(term, MARGIN = 2, paste, collapse = ", ")
+
+# first topic identified for every document (tweet)
+library(data.table) #fore IDate
+topic <- topics(lda, 1)
+topics <- data.frame(date=as.IDate(tweets.df$created), topic)
+qplot(date, ..count.., data=topics, geom="density",
+      fill=term[topic], position="stack")
